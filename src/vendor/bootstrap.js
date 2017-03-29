7655a8f9c53300520c15af606e7fde5662215ef8
AVykhrystyuk <AVykhrystyuk@gmail.com>
2017-03-29 16:14:50 +0500

    aligned search for mobile

--- src/vendor/bootstrap.js
+++ src/vendor/bootstrap.js
@@ -1,7 +1,8 @@
-import './bootswatch.less'
+import './bootswatch.less';
+import './bootstrap.extensions.less';
 
-//import 'bootstrap/less/bootstrap.less'
-//import 'bootstrap/less/theme.less'
+//import 'bootstrap/less/bootstrap.less';
+//import 'bootstrap/less/theme.less';
 
 //import 'bootstrap/dist/css/bootstrap.css';
 //import 'bootstrap/dist/css/bootstrap-theme.css';
