a6a0e4c5a8bc44c2ca5b0e9b3acef0f42bcdfc22
AVykhrystyuk <AVykhrystyuk@gmail.com>
2017-04-01 18:42:16 +0500

    updated server.js

--- src/server.js
+++ src/server.js
@@ -9,35 +9,9 @@ const app = express();
 app.use(compression());
 app.use(express.static(path.resolve('build')));
 
-const config = buildConfig();
-app.listen(config.port, config.ipAddress, function() {
-    var url = `http://${config.ipAddress}:${config.port}`;
-    console.log(`Your server is listening on port ${config.port} (${url})`);
+app.listen(process.env.PORT || 3000, function() {
+    let address = this.address();
+    console.log(address);
+    let url = `http://localhost:${address.port}`;
+    console.log(`Server listening on port ${address.port} (${url})`);
 });
-
-function buildConfig() {
-    return {
-        ipAddress: "127.0.0.1",
-        port: process.env.PORT || 3000
-    };
-}
-
-/*
-function buildOpenshiftConfig() {
-    let ip = process.env.OPENSHIFT_NODEJS_IP;
-    let port = process.env.OPENSHIFT_NODEJS_PORT;
-
-    let config = buildConfig();
-    if (ip) {
-        config.ipAddress = ip;
-    } else {
-        console.warn(`No OPENSHIFT_NODEJS_IP var, using ${config.ipAddress}`)
-    }
-
-    if (port) {
-        config.port = port;
-    };
-
-    return config;
-}
-*/
