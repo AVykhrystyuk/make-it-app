0383ae45b387531cd28ea215041901e5957fbeab
AVykhrystyuk <AVykhrystyuk@gmail.com>
2017-04-07 18:53:57 +0500

    started serve google fonts locally

--- src/vendor/bootstrap/index.js
+++ src/vendor/bootstrap/index.js
@@ -1,4 +1,4 @@
-import './bootswatch.less';
+import './bootstrap.less';
 
 //import 'bootstrap/less/bootstrap.less';
 //import 'bootstrap/less/theme.less';
--- src/vendor/index.js
+++ src/vendor/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import './bootstrap.js'
+import './bootstrap'
 
 import 'angular';
 import 'angular-ui-router';
